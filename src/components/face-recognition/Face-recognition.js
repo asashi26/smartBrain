1bc8c2dab5eeb81db6bef0897888e1d6ccfcdf85
Marta Kocmidova <mochomurka6@seznam.cz>
2019-05-29 08:10:58 +0200

    face detection box

--- src/components/face-recognition/Face-recognition.js
+++ src/components/face-recognition/Face-recognition.js
@@ -8,7 +8,7 @@ const FaceRecognition = ({ imageUrl, box }) => {
     <div className='center'>
     <div className='absolute mt2'>
       <img id='inputimage' src={imageUrl} alt='#' width='500px' height='auto'/>
-      <div className='bouning-box'></div>
+      <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol }}></div>
     </div>
       
     </div>
